06101d89a921d9955675952f98f39582cacce91a
rkirby48 <85845629+rkirby48@users.noreply.github.com>
2021-07-12 16:49:56 -0400

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -6,7 +6,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import seaborn as sns
+#import seaborn as sns
 from transformers import AutoModelForSequenceClassification
 from transformers import TFAutoModelForSequenceClassification
 from transformers import AutoTokenizer
