1ea18af0b142122ad44462d04526749942f67914
rkirby48 <85845629+rkirby48@users.noreply.github.com>
2021-07-18 15:57:40 -0400

    Add files via upload

--- Streamlit.py
+++ Streamlit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+#Import packages
 import streamlit as st
 import altair as alt
 import time
@@ -28,6 +28,7 @@ def preprocess(text):
         new_text.append(t)
     return " ".join(new_text)
 
+#Load sentiment model
 task='sentiment'
 MODEL = f"cardiffnlp/twitter-roberta-base-sentiment"
 tokenizer = AutoTokenizer.from_pretrained(MODEL)
@@ -42,8 +43,9 @@ labels = [row[1] for row in csvreader if len(row) > 1]
 model = AutoModelForSequenceClassification.from_pretrained(MODEL)
 model.save_pretrained(MODEL)
 tokenizer.save_pretrained(MODEL)
-# In[2]:
 
+
+#Cache function to pick airline based on user input tweet
 @st.cache(allow_output_mutation=True)
 def checkairline(Tweet):
     if "@AmericanAir" in Tweet:
@@ -58,24 +60,25 @@ def checkairline(Tweet):
         airlinecheck = "JetBlue"
     return airlinecheck
 
-new_data = pd.read_csv('https://github.com/BUAD625-Team4-2021/New_Tweets/blob/main/new_tweets_final_sentiment.csv?raw=true',index_col=0)
+#Load in new tweet data from Github
+new_data = pd.read_csv('https://github.com/BUAD625-Team4-2021/New_Tweets/blob/main/new_tweets_final_sentiment.csv?raw=true',index_col=[0])
+new_data = new_data.drop("Unnamed: 0.1",1)
 
-# In[3]:
 
+#Create cache to hold all user input tweets
 @st.cache(allow_output_mutation=True)
 def get_data():
     return []
 
-# In[ ]:
+
+#Title and subtitle for streamlit
 st.title('Airline Sentiment Analysis')
 st.subheader("Enter a Tweet in order to receive the aspect and sentiment")
 
-#user_df = [["test"],["user"]]
-
-
+#Textbox for user input
 user_input = st.text_input("Input Tweet")
 
-
+#If button is pressed, analyze the tweet that has been entered into the text box
 if st.button("Analyze Tweet"):
     for tweet in user_input:
     
@@ -90,22 +93,21 @@ if st.button("Analyze Tweet"):
         for i in range(scores.shape[0]):
             l = labels[ranking[i]]
             s = scores[ranking[i]]
-            #print(f"{i+1}) {l} {np.round(float(s), 4)}")
 
         final_score = labels[ranking[0]]
         airlinecheck = checkairline(user_input)
    
         
-    
     get_data().append({"Tweet": user_input, "Airline": airlinecheck, "Sentiment": final_score, "Aspect": "aspect"})
-    #test_data.append({"Tweet": user_input, "Airline": airlinecheck, "Sentiment": final_score, "Aspect": "aspect"}, ignore_index=True)
-
+    
+#Combine data loaded from Github and new tweets entered by user
 user_input_data = pd.DataFrame(get_data(), columns =['Tweet', 'Airline', 'Sentiment','Aspect'], dtype = float)
-
 test_data = pd.concat([new_data,user_input_data]).reset_index(drop=True)
 
+#Show full results from most recent tweet
 st.write(test_data.iloc[-1])
 
+#Colors for graphs
 sns.set_palette("muted")
 sns.set(font_scale=.75)
 
@@ -113,6 +115,7 @@ palette = {"positive":"#078112",
            "negative":"#AD3108", 
            "neutral":"#A59F9D"}
 
+#Tweet distirbution graph
 fig = plt.figure()
 plot1 = sns.catplot(x = "Airline",       # x variable name
             hue = "Sentiment",  # group variable name
@@ -124,11 +127,12 @@ plt.ylabel('Count of Tweets')
 plt.title('Tweet Distribution')
 st.pyplot(fig=plot1)
 
+#Dropdown for Aspect Anlysis graph
 option = st.selectbox(
     'Filter Data to Selected Airline',
     ('AmericanAir', 'SouthwestAir', 'Delta', 'united', 'JetBlue' ))
 
-
+#Aspect Anlysis graph
 plot2_data = test_data.loc[(test_data['Airline'] == option)]
 
 palette = {"positive":"#078112",
@@ -142,7 +146,7 @@ plt.xlabel('Aspect')
 plt.title('Aspect Analysis for '+ option)
 st.pyplot(fig=plot2)
 
-
+#Print full tweet database
 st.subheader("Full List of Tweets")
 st.dataframe(test_data,height = 300)
 
